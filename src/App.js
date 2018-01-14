03ebdbebb754f0288875c85a0a8e6e815f23194f
Reena Patil <reena.patil@outlook.com>
2018-01-15 10:23:52 +1100

    login page

---
+++ src/App.js
@@ -0,0 +1,50 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Top from './Components/Top';
+import Private from './Components/Private';
+import Login from './Auth/Login';
+import Logout from './Auth/Logout';
+
+const App = () => {
+  return(
+    <div>
+      <Header />
+      <main>
+        <Switch>
+        <Route exact path="/" component={Top} />
+        <Route path="/private" component={Private} />
+        <Route path="/login" component={Login} />
+        <Route path="logout" component={Logout} />
+        </Switch>
+      </main>
+    </div>
+  )
+}
+
+export default App
+
