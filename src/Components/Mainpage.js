069ff38899f229c5e928a1e20b725eb3eb164fdf
kasumi <kasumi0201@gmail.com>
2018-01-15 12:08:36 +1100

    added pictures.

---
+++ src/Components/Mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Mainpage = () => (
+  <div>
+    <h2>hello</h2>
+    <p>Hi,</p>
+    <p>here is the main page.</p>
+    <div className="mainpic_div">
+          <img src="mainpic.jpg" alt="logo" className="mainpic" width={800}/>
+    </div>
+  </div>
+)
+
+export default Mainpage;
