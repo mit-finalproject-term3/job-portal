3b4c694e182f6cc2ee177a8c89b21a619554a0ce
kasumi <kasumi0201@gmail.com>
2018-01-16 18:49:16 +1100

    Edit Top page using Ant Design

--- src/Components/Top.js
+++ src/Components/Top.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { List, Card } from 'antd';
 
+const data = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+  {
+    title: 'Title 5',
+  },
+  {
+    title: 'Title 6',
+  },
+  {
+    title: 'Title 7',
+  },
+  {
+    title: 'Title 8',
+  },
+];
 
 const Top = () => (
   <div>
-    <h2>Top Page</h2>
-    <p>Hi,</p>
-    <p>If you are seeing this message, your authentication app is properly running. Please keep on checking by clicking the link buttons inside the header, which were generated from the <em>Link</em> element of React Router.</p>
+    <br/>
+      <h2>Top Page</h2>
+    <br/>
+    <div className="mainpic_div">
+       <img src="mainpic.jpg" alt="mainpic" className="mainpic" width={800}/>
+    </div>
+    <br/>
+      <List
+      grid={{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6 }}
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <Card title={item.title}>
+          Card content
+          <br/>
+          <Link to="/mainpage">click here</Link>
+          </Card>
+        </List.Item>
+      )}
+    />
   </div>
 )
 
